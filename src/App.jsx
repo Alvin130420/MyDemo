4a8eb0d9550f9fc3c8cc7830ce6fb5c8f2781f28
Salieh Kadiri <salieh7787@gmail.com>
2025-05-07 17:56:09 +0100

    Add the FAQs Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import './App.css'
 import Navbar from './pages/Navbar'
 import Footer from './pages/Footer'
 import AppNavigation from "../src/route/AppNavigation"
+import FAQSections from './components/FAQSections'
 function App() {
  return(
   <div>
     <Navbar/>
     <AppNavigation />
+    <FAQSections />
     <Footer/>
+
+
   </div>
  )
 }
