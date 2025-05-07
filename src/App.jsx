3cd4e205a38ddd005e2b38d0b1abefc368ce337f
Tahir Musa <tahirmusa979@gmail.com>
2025-05-07 16:01:23 +0200

    I don edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AppNavigation from './components/AppNavigation'
+import AppNavigation from './route/AppNavigation'
 function App() {
  return(
   <div>
