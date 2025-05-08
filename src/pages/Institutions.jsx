01ac642df43131e6a157c212b1c110f070209c90
meenore <achimuguunekwuojoamina@gmail.com>
2025-05-08 10:06:35 +0100

    pricing and institutions component

---
+++ src/pages/Institutions.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Institutions(){
+   return(
+    <div className="bg-gray-50">
+    <h1 className="text-2xl  font-bold text-indigo-900 text-center mb-3">Trusted by Leading Institutions</h1>
+    <p className="text-semi-bold text-gray-500 text-center">Join thousands of schools and universities already using Elite Edu Tech</p>
+    <div className="flex justify-center p-10">
+    <div className=" grid grid-cols-1 gap-8 grid-cols-1 md:grid-cols-3 lg:grid-cols-6 ">
+        <div className="bg-gray-400 text-white font-semi-bold text-center p-4 w-30">University A</div>
+        <div className="bg-gray-400 text-white text-center p-4 w-32">College B</div>
+        <div className="bg-gray-400 text-white text-center p-4 w-32">School C</div>
+        <div className="bg-gray-400 text-white text-center p-4 w-32">Academy D</div>
+        <div className="bg-gray-400 text-white text-center p-4 w-32">Institute E</div>
+        <div className="bg-gray-400 text-white text-center p-4 w-32">Learning F</div>
+    </div>
+    </div>
+</div>
+   )
+}
+export default Institutions;
--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -2,6 +2,8 @@ import Navbar from "../pages/Navbar"
 import Hero from "../pages/Hero"
 import NotFound from "../pages/NotFound"
 import Footer from "../pages/Footer"
+import Pricing from "../pages/Pricing"
+import Institutions from "../pages/Institutions"
 
 
 
@@ -16,5 +18,7 @@ const routes = [
         ] ,
        },
     { path: "*", element: <NotFound />, },
+    { path: "pricing", element: <Pricing />, },
+    { path: "institutions", element: <Institutions />, }
 ]
 export default routes
