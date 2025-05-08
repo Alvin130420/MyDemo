32bdfcc032fcdb0876ec7146e0fd07f086fc9b0f
Alvin <abyakator647@gmail.com>
2025-05-08 11:00:43 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Navbar from './pages/Navbar'
 import Footer from './pages/Footer'
 import AppNavigation from "../src/route/AppNavigation"
 import FAQSections from "../src/components/FAQSections"
+import Pricing from '../src/pages/Pricing'
+import Institutions from '../src/pages/Institutions' 
 function App() {
  return(
   <div>
     <Navbar/>
     <AppNavigation />
+    <Pricing/>
+    <Institutions/>
     <FAQSections />
     <Footer/>
 
