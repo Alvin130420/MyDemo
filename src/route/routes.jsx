6a38194f64b46b8553f0d0001c8aea9a8a43d1f0
Alvin <abyakator647@gmail.com>
2025-05-08 10:27:19 +0100

    save

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,13 +1,9 @@
 import Hero from "../pages/Hero"
 import NotFound from "../pages/NotFound"
-import Navbar from "../pages/Navbar"
-import { Children } from "react"
 
 
 const routes = [
-    { path: "/", element:<Hero/>, 
-        children:[{path:"/", element:<Hero/>}]
-     },
+    { path: "", element:<Hero/>},
     { path: "*", element: <NotFound />, },
 ]
 export default routes
