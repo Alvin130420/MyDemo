0132f220aa8238d573266a8937b853ee16161f20
Alvin <abyakator647@gmail.com>
2025-05-08 11:36:48 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import './App.css'
 import Navbar from './pages/Navbar'
 import Footer from './pages/Footer'
 import AppNavigation from "../src/route/AppNavigation"
+import  Features  from '../src/pages/Features'
+import About from '../src/pages/About'
+import Pricing from '../src/pages/Pricing'
+import Institutions from '../src/pages/Institutions' 
+import FAQSections from '../src/components/FAQSections'
 function App() {
  return(
   <div>
     <Navbar/>
     <AppNavigation />
+    <About/>
+    <Features/>
+    <Pricing/>
+    <Institutions/>
     <FAQSections />
     <Footer/>
 
